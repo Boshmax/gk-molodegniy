290eb2122a751fd1636887c51fc10102b9db866d
BoshMax <BoshMax@gmail.com>
2014-10-12 12:42:20 +0400

    11

---
+++ Init/init_House1.sql
@@ -0,0 +1,415 @@
+﻿/*
+$Date: 15.09.2014 13:08:31 $
+$Source: Git\gk-molodegniy\Init\tUser.sql $
+
+Назначение:
+
+Пример вызова:
+
+
+
+*/
+declare @strString_ varchar(max)=
+'[b]Молодежный I[/b]
+ 
+
+[table layout=fixed width=426px]
+[tr]
+[td width=30px bgcolor=#84A577][color=white]Этаж[/color][/td]
+[td colspan=12 bgcolor=#84A577][align=center][color=white]Номер на площадке[/color][/align][/td]
+[/tr]
+[tr]
+[td] # [/td]
+[td]1[/td]
+[td]2[/td]
+[td]3[/td]
+[td]4[/td]
+[td]5[/td]
+[td]6[/td]
+[td]7[/td]
+[td]8[/td]
+[td]9[/td]
+[td]10[/td]
+[td]11[/td]
+[td]12[/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/x27rn.png]20[/url][/td]
+[td][color=#F4FADB]209[/color][/td]
+[td][color=#F4FADB]210[/color][/td]
+[td][color=#F4FADB]211[/color][/td]
+[td][color=#F4FADB]212[/color][/td]
+[td][color=#F4FADB]213[/color][/td]
+[td][color=#F4FADB]214[/color][/td]
+[td][color=#F4FADB]215[/color][/td]
+[td][abbr="Янина"][url=http://gk-molodegniy.ru/profile.php?id=747]216[/url][/abbr] [/td]
+[td][color=#F4FADB]217[/color][/td]
+[td][color=#F4FADB]218[/color][/td]
+[td][color=#F4FADB]219[/color][/td]
+[td][abbr="Anastasia"][url=http://gk-molodegniy.ru/profile.php?id=565]220[/url][/abbr] [/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/x27rn.png]19[/url][/td]
+[td][abbr="olga194"][url=http://gk-molodegniy.ru/profile.php?id=534]197[/url][/abbr] [/td]
+[td][color=#F4FADB]198[/color][/td]
+[td][color=#F4FADB]199[/color][/td]
+[td][abbr="Dim"][url=http://gk-molodegniy.ru/profile.php?id=488]200[/url][/abbr] [/td]
+[td][color=#F4FADB]201[/color][/td]
+[td][color=#F4FADB]202[/color][/td]
+[td][abbr="Spell"][url=http://gk-molodegniy.ru/profile.php?id=513]203[/url][/abbr] [/td]
+[td][color=#F4FADB]204[/color][/td]
+[td][abbr="Natalie"][url=http://gk-molodegniy.ru/profile.php?id=509]205[/url][/abbr] [/td]
+[td][abbr="Влад"][url=http://gk-molodegniy.ru/profile.php?id=645]206[/url][/abbr] [/td]
+[td][abbr="DaGri"][url=http://gk-molodegniy.ru/profile.php?id=98]207[/url][/abbr] [/td]
+[td][abbr="Alexey"][url=http://gk-molodegniy.ru/profile.php?id=481]208[/url][/abbr] [/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/x27rn.png]18[/url][/td]
+[td][color=#F4FADB]185[/color][/td]
+[td][color=#F4FADB]186[/color][/td]
+[td][abbr="ММария"][url=http://gk-molodegniy.ru/profile.php?id=154]187[/url][/abbr] [/td]
+[td][abbr="Маргарита"][url=http://gk-molodegniy.ru/profile.php?id=391]188[/url][/abbr] [/td]
+[td][abbr="владимир"][url=http://gk-molodegniy.ru/profile.php?id=370]189[/url][/abbr] [/td]
+[td][color=#F4FADB]190[/color][/td]
+[td][color=#F4FADB]191[/color][/td]
+[td][color=#F4FADB]192[/color][/td]
+[td][color=#F4FADB]193[/color][/td]
+[td][color=#F4FADB]194[/color][/td]
+[td][abbr="ННН"][url=http://gk-molodegniy.ru/profile.php?id=653]195[/url][/abbr] [/td]
+[td][abbr="Евгения"][url=http://gk-molodegniy.ru/profile.php?id=102]196[/url][/abbr] [/td]
+[/tr]
+[tr]
+[td][url=http://s5.uploads.ru/JjBpX.png]17[/url][/td]
+[td][color=#F4FADB]173[/color][/td]
+[td][color=#F4FADB]174[/color][/td]
+[td][color=#F4FADB]175[/color][/td]
+[td][color=#F4FADB]176[/color][/td]
+[td][color=#F4FADB]177[/color][/td]
+[td][abbr="Alexander"][url=http://gk-molodegniy.ru/profile.php?id=336]178[/url][/abbr] [/td]
+[td][abbr="Kostya"][url=http://gk-molodegniy.ru/profile.php?id=336]179[/url][/abbr] [/td]
+[td][color=#F4FADB]180[/color][/td]
+[td][color=#F4FADB]181[/color][/td]
+[td][color=#F4FADB]182[/color][/td]
+[td][color=#F4FADB]183[/color][/td]
+[td][color=#F4FADB]184[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s5.uploads.ru/JjBpX.png]16[/url][/td]
+[td][abbr="Ксю"][url=http://gk-molodegniy.ru/profile.php?id=145]161[/url][/abbr] [abbr="Дмитрий"][url=http://gk-molodegniy.ru/profile.php?id=215]*[/url][/abbr] [/td]
+[td][color=#F4FADB]162[/color][/td]
+[td][color=#F4FADB]163[/color][/td]
+[td][abbr="Виктор"][url=http://gk-molodegniy.ru/profile.php?id=325]164[/url][/abbr] [/td]
+[td][abbr="Наталья Жадан"][url=http://gk-molodegniy.ru/profile.php?id=591]165[/url][/abbr] [/td]
+[td][abbr="Sol"][url=http://gk-molodegniy.ru/profile.php?id=535]166[/url][/abbr] [/td]
+[td][color=#F4FADB]167[/color][/td]
+[td][color=#F4FADB]168[/color][/td]
+[td][color=#F4FADB]169[/color][/td]
+[td][color=#F4FADB]170[/color][/td]
+[td][color=#F4FADB]171[/color][/td]
+[td][abbr="Marina"][url=http://gk-molodegniy.ru/profile.php?id=146]172[/url][/abbr] [abbr="Роман"][url=http://gk-molodegniy.ru/profile.php?id=314]*[/url][/abbr] [/td]
+[/tr]
+[tr]
+[td][url=http://s5.uploads.ru/JjBpX.png]15[/url][/td]
+[td][color=#F4FADB]149[/color][/td]
+[td][color=#F4FADB]150[/color][/td]
+[td][color=#F4FADB]151[/color][/td]
+[td][abbr="DLeonid"][url=http://gk-molodegniy.ru/profile.php?id=508]152[/url][/abbr] [abbr="Pinki"][url=http://gk-molodegniy.ru/profile.php?id=429]*[/url][/abbr] [/td]
+[td][abbr="KLarisa"][url=http://gk-molodegniy.ru/profile.php?id=258]153[/url][/abbr] [/td]
+[td][abbr="Петр"][url=http://gk-molodegniy.ru/profile.php?id=567]154[/url][/abbr] [/td]
+[td][color=#F4FADB]155[/color][/td]
+[td][color=#F4FADB]156[/color][/td]
+[td][color=#F4FADB]157[/color][/td]
+[td][color=#F4FADB]158[/color][/td]
+[td][color=#F4FADB]159[/color][/td]
+[td][color=#F4FADB]160[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s5.uploads.ru/JjBpX.png]14[/url][/td]
+[td][color=#F4FADB]137[/color][/td]
+[td][color=#F4FADB]138[/color][/td]
+[td][color=#F4FADB]139[/color][/td]
+[td][color=#F4FADB]140[/color] [/td]
+[td][abbr="Svetlana"][url=http://gk-molodegniy.ru/profile.php?id=618]141[/url][/abbr][/td]
+[td][color=#F4FADB]142[/color][/td]
+[td][abbr="Tan4ik"][url=http://gk-molodegniy.ru/profile.php?id=556]143[/url][/abbr] [/td]
+[td][color=#F4FADB]144[/color][/td]
+[td][abbr="ЕленаД"][url=http://gk-molodegniy.ru/profile.php?id=281]145[/url][/abbr] [/td]
+[td][color=#F4FADB]146[/color][/td]
+[td][abbr="irochka_gr"][url=http://gk-molodegniy.ru/profile.php?id=315]147[/url][/abbr] [/td]
+[td][color=#F4FADB]148[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s5.uploads.ru/E9DA7.png]13[/url][/td]
+[td][color=#F4FADB]125[/color][/td]
+[td][abbr="Tempax"][url=http://gk-molodegniy.ru/profile.php?id=281]126[/url][/abbr] [abbr="Ксения"][url=http://gk-molodegniy.ru/profile.php?id=119]*[/url][/abbr] [/td]
+[td][color=#F4FADB]127[/color][/td]
+[td][color=#F4FADB]128[/color][/td]
+[td][color=#F4FADB]129[/color][/td]
+[td][color=#F4FADB]130[/color][/td]
+[td][abbr="Irish@cream"][url=http://gk-molodegniy.ru/profile.php?id=134]131[/url][/abbr] [/td]
+[td][abbr="Татьяна_1964"][url=http://gk-molodegniy.ru/profile.php?id=598]132[/url][/abbr] [/td]
+[td][color=#F4FADB]133[/color][/td]
+[td][color=#F4FADB]134[/color][/td]
+[td][color=#F4FADB]135[/color][/td]
+[td][color=#F4FADB]136[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s5.uploads.ru/E9DA7.png]12[/url][/td]
+[td][color=#F4FADB]113[/color][/td]
+[td][color=#F4FADB]114[/color][/td]
+[td][color=#F4FADB]115[/color][/td]
+[td][color=#F4FADB]116[/color][/td]
+[td][color=#F4FADB]117[/color][/td]
+[td][color=#F4FADB]118[/color][/td]
+[td][abbr="Platin112"][url=http://gk-molodegniy.ru/profile.php?id=486]119[/url][/abbr] [/td]
+[td][color=#F4FADB]120[/color][/td]
+[td][color=#F4FADB]121[/color][/td]
+[td][color=#F4FADB]122[/color][/td]
+[td][color=#F4FADB]123[/color][/td]
+[td][color=#F4FADB]124[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s5.uploads.ru/E9DA7.png]11[/url][/td]
+[td][color=#F4FADB]101[/color][/td]
+[td][abbr="Hooligan"][url=http://gk-molodegniy.ru/profile.php?id=17]102[/url][/abbr] [/td]
+[td][color=#F4FADB]103[/color][/td]
+[td][abbr="Vita"][url=http://gk-molodegniy.ru/profile.php?id=219]104[/url][/abbr] [/td]
+[td][abbr="Кентавр"][url=http://gk-molodegniy.ru/profile.php?id=386]105[/url][/abbr] [/td]
+[td][abbr="Bax"][url=http://gk-molodegniy.ru/profile.php?id=479]106[/url][/abbr] [/td]
+[td][color=#F4FADB]107[/color][/td]
+[td][abbr="Lucker"][url=http://gk-molodegniy.ru/profile.php?id=548]108[/url][/abbr] [/td]
+[td][color=#F4FADB]109[/color][/td]
+[td][abbr="Vlad"][url=http://gk-molodegniy.ru/profile.php?id=570]110[/url][/abbr] [/td]
+[td][color=#F4FADB]111[/color][/td]
+[td][color=#F4FADB]112[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s5.uploads.ru/E9DA7.png]10[/url][/td]
+[td][color=#F4FADB]89[/color][/td]
+[td][color=#F4FADB]90[/color][/td]
+[td][abbr="Татьяна Z"][url=http://gk-molodegniy.ru/profile.php?id=485]91[/url][/abbr] [/td]
+[td][color=#F4FADB]92[/color][/td]
+[td][color=#F4FADB]93[/color][/td]
+[td][color=#F4FADB]94[/color][/td]
+[td][color=#F4FADB]95[/color][/td]
+[td][color=#F4FADB]96[/color][/td]
+[td][color=#F4FADB]97[/color] [/td]
+[td][color=#F4FADB]98[/color][/td]
+[td][color=#F4FADB]99[/color][/td]
+[td][color=#F4FADB]100[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/6haKT.png]9[/url][/td]
+[td][abbr="and1"][url=http://gk-molodegniy.ru/profile.php?id=638]77[/url][/abbr] [/td]
+[td][abbr="trolnik"][url=http://gk-molodegniy.ru/profile.php?id=172]78[/url][/abbr] [/td]
+[td][color=#F4FADB]79[/color][/td]
+[td][abbr="XAHCEP"][url=http://gk-molodegniy.ru/profile.php?id=39]80[/url][/abbr] [/td]
+[td][abbr="Ирэн"][url=http://gk-molodegniy.ru/profile.php?id=4]81[/url][/abbr] [/td]
+[td][color=#F4FADB]82[/color][/td]
+[td][color=#F4FADB]83[/color][/td]
+[td][color=#F4FADB]84[/color][/td]
+[td][color=#F4FADB]85[/color][/td]
+[td][color=#F4FADB]86[/color][/td]
+[td][color=#F4FADB]87[/color][/td]
+[td][color=#F4FADB]88[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/6haKT.png]8[/url][/td]
+[td][color=#F4FADB]65[/color][/td]
+[td][color=#F4FADB]66[/color][/td]
+[td][color=#F4FADB]67[/color][/td]
+[td][abbr="Полина"][url=http://gk-molodegniy.ru/profile.php?id=40]68[/url][/abbr] [/td]
+[td][color=#F4FADB]69[/color][/td]
+[td][abbr="Alexander L"][url=http://gk-molodegniy.ru/profile.php?id=159]70[/url][/abbr] [/td]
+[td][color=#F4FADB]71[/color][/td]
+[td] [abbr="Gaika"][url=http://gk-molodegniy.ru/profile.php?id=797]72[/url][/abbr][/td]
+[td][color=#F4FADB]73[/color][/td]
+[td][abbr="Алёна"][url=http://gk-molodegniy.ru/profile.php?id=482]74[/url][/abbr] [/td]
+[td][color=#F4FADB]75[/color][/td]
+[td][color=#F4FADB]76[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/6haKT.png]7[/url][/td]
+[td][abbr="КАРЛСОН"][url=http://gk-molodegniy.ru/profile.php?id=197]53[/url][/abbr] [/td]
+[td][abbr="Anna"][url=http://gk-molodegniy.ru/profile.php?id=504]54[/url][/abbr] [/td]
+[td][color=#F4FADB]55[/color][/td]
+[td][abbr="ferdgi"][url=http://gk-molodegniy.ru/profile.php?id=560]56[/url][/abbr] [/td]
+[td][color=#F4FADB]57[/color][/td]
+[td][abbr="арслан"][url=http://gk-molodegniy.ru/profile.php?id=345]58[/url][/abbr] [/td]
+[td][abbr="njcobra"][url=http://gk-molodegniy.ru/profile.php?id=233]59[/url][/abbr] [/td]
+[td][color=#F4FADB]60[/color][/td]
+[td][abbr="zlaya_takca"][url=http://gk-molodegniy.ru/profile.php?id=282]61[/url][/abbr] [/td]
+[td][abbr="Виталий"][url=http://gk-molodegniy.ru/profile.php?id=594]62[/url][/abbr] [/td]
+[td][color=#F4FADB]63[/color][/td]
+[td][color=#F4FADB]64[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/6haKT.png]6[/url][/td]
+[td][abbr="Вадим"][url=http://gk-molodegniy.ru/profile.php?id=327]41[/url][/abbr] [/td]
+[td][abbr="AnitaBlok"][url=http://gk-molodegniy.ru/profile.php?id=609]42[/url][/abbr] [/td]
+[td][color=#F4FADB]43[/color][/td]
+[td][color=#F4FADB]44[/color][/td]
+[td][color=#F4FADB]45[/color][/td]
+[td][color=#F4FADB]46[/color][/td]
+[td][color=#F4FADB]47[/color][/td]
+[td][color=#F4FADB]48[/color][/td]
+[td][abbr="MTI"][url=http://gk-molodegniy.ru/profile.php?id=54]49[/url][/abbr] [/td]
+[td][color=#F4FADB]50[/color][/td]
+[td][color=#F4FADB]51[/color][/td]
+[td][color=#F4FADB]52[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/6haKT.png]5[/url][/td]
+[td][abbr="Арсений"][url=http://gk-molodegniy.ru/profile.php?id=522]29[/url][/abbr] [/td]
+[td][abbr="куку"][url=http://gk-molodegniy.ru/profile.php?id=339]30[/url][/abbr] [/td]
+[td][color=#F4FADB]31[/color][/td]
+[td][color=#F4FADB]32[/color][/td]
+[td][color=#F4FADB]33[/color][/td]
+[td][color=#F4FADB]34[/color][/td]
+[td][color=#F4FADB]35[/color][/td]
+[td][color=#F4FADB]36[/color][/td]
+[td][color=#F4FADB]37[/color][/td]
+[td][color=#F4FADB]38[/color][/td]
+[td][color=#F4FADB]39[/color][/td]
+[td][color=#F4FADB]40[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/6haKT.png]4[/url][/td]
+[td][abbr="Pand"][url=http://gk-molodegniy.ru/profile.php?id=602]17[/url][/abbr] [/td]
+[td][abbr="Оксана"][url=http://gk-molodegniy.ru/profile.php?id=572]18[/url][/abbr] [/td]
+[td][abbr="Павел"][url=http://gk-molodegniy.ru/profile.php?id=214]19[/url][/abbr] [/td]
+[td][color=#F4FADB]20[/color][/td]
+[td][color=#F4FADB]21[/color][/td]
+[td][color=#F4FADB]22[/color][/td]
+[td][abbr="Alexxx"][url=http://gk-molodegniy.ru/profile.php?id=408]23[/url][/abbr] [/td]
+[td][color=#F4FADB]24[/color][/td]
+[td][abbr="чиполло"][url=http://gk-molodegniy.ru/profile.php?id=90]25[/url][/abbr] [/td]
+[td][color=#F4FADB]26[/color][/td]
+[td][color=#F4FADB]27[/color][/td]
+[td][color=#F4FADB]28[/color][/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/GYzCB.png]3[/url][/td]
+[td][color=#F4FADB]9[/color][/td]
+[td][abbr="Vladimir Strukov"][url=http://gk-molodegniy.ru/profile.php?id=577]A11[/url][/abbr] [/td]
+[td][abbr="tatishka"][url=http://gk-molodegniy.ru/profile.php?id=322]A12[/url][/abbr][/td]
+[td]A[/td]
+[td][color=#F4FADB]10[/color] [/td]
+[td][abbr="Romber"][url=http://gk-molodegniy.ru/profile.php?id=324]11[/url][/abbr] [/td]
+[td][color=#F4FADB]12[/color][/td]
+[td][abbr="Александр и Ольга"][url=http://gk-molodegniy.ru/profile.php?id=422]13[/url][/abbr] [/td]
+[td][color=#F4FADB]14[/color][/td]
+[td][abbr="Lera"][url=http://gk-molodegniy.ru/profile.php?id=596]15[/url][/abbr] [/td]
+[td][color=#F4FADB]16[/color][/td]
+
+[td]X[/td]
+[/tr]
+[tr]
+[td][url=http://s4.uploads.ru/GYzCB.png]2[/url][/td]
+[td][abbr="KeHu"][url=http://gk-molodegniy.ru/profile.php?id=393]1[/url][/abbr] [/td]
+[td]A[/td]
+[td][abbr="Максим"][url=http://gk-molodegniy.ru/profile.php?id=354]A9[/url][/abbr][/td]
+[td]A[/td]
+[td][color=#F4FADB]2[/color][/td]
+[td][color=#F4FADB]3[/color][/td]
+[td][color=#F4FADB]4[/color][/td]
+[td][color=#F4FADB]5[/color][/td]
+[td][color=#F4FADB]6[/color][/td]
+[td][color=#F4FADB]7[/color][/td]
+[td][abbr="SergeyKh"][url=http://gk-molodegniy.ru/profile.php?id=575]8[/url][/abbr] [/td]
+[td]X[/td]
+[/tr]
+[tr]
+[td]1[/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[td][color=#F4FADB]0[/color][/td]
+[/tr]
+[/table]
+ 
+[spoiler="Список жильцов"]Alexander L (№ 70)
+Alexey (№ 208)
+Alexxx (№ 23)
+Anastasia (№ 220)
+AnitaBlok (№ 42)
+Anna (№ 54)
+Bax (№ 106)
+DaGri (№ 207)
+Dim(№ 200)
+DLeonid (№ 152)
+ferdgi (№ 56)
+Hooligan (№ 102)
+Irish@cream (№ 131)
+KeHu (№ 1)
+KLarisa (№ 153)
+Kostya (№ 179)
+Lera (№ 15)
+Lucker (№ 108)
+Marina (№ 172)
+MTI (№ 49)
+Natalie (№ 205)
+njcobra (№ 59)
+olga194 (№ 197)
+Pinki (№ 152)
+Platin112 (№ 119)
+Romber (№ 11)
+SergeyKh (№ 8)
+Sol (№ 166)
+Spell (№ 203)
+Svetlana (№ 140)
+Tan4ik (№ 143)
+tatishka (№ 4)
+Tempax (№ 126)
+trolnik (№ 78)
+Vita (№ 104)
+Vlad (№ 110)
+Vladimir Strukov (№ 10)
+zlaya_takca (№ 61)
+Александр и Ольга (№ 13)
+Алёна (№ 74)
+Арсений (№ 29)
+арслан (№ 58)
+Вадим (№ 41)
+Виктор (№ 164)
+Виталий (№ 62)
+Влад (№ 206)
+владимир (№ 189)
+Дмитрий (№ 161)
+Евгения (№ 196)
+ЕленаД (№ 145)
+Ирэн (№ 81)
+КАРЛСОН (№ 53)
+Кентавр (№ 105)
+Ксения (№ 126)
+Ксю (№ 161)
+куку (№ 30)
+Максим (№ 1)
+Маргарита (№ 188)
+ММария (№ 187)
+Наталья Жадан (№ 165)
+ННН (№ 195)
+Оксана (№ 18)
+Павел (№ 19)
+Петр (№ 154)
+Полина (№ 68)
+Роман (№ 172)
+Татьяна Z (№ 97)
+Татьяна_1964 (№ 132)
+чиполло (№ 25)
+
+[/spoiler]
+Для добавления указываем Этаж, Квартиру'
+
+
+exec dbo.pParseChessFloor
+	@strString = @strString_
+,	@debug = 0
